ab92d5510b5de86ad9d9f88157c63fd95b3cf643
pietromosca03 <s312742@studenti.polito.it>
2025-04-01 14:46:01 +0200

    first commit

---
+++ Exercise2/Exercise2/Debug/CMakeFiles/MeanValue.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for MeanValue.
+# This may be replaced when dependencies are built.
---
+++ Exercise2/Exercise2/Debug/CMakeFiles/MeanValue.dir/main.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/MeanValue.dir/main.cpp.o: \
+ /home/appuser/Data/Exercise2/Exercise2/main.cpp \
+ /usr/include/stdc-predef.h
